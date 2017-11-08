1293401edfbd63e275fde25c8214988a17288ccc
FilipElen <filipelengit@outlook.com>
2017-11-08 15:48:29 +0100

    add and search on chouchdb part 1, not debugged

--- app/_attachments/scripts/app.js
+++ app/_attachments/scripts/app.js
@@ -3,41 +3,83 @@
 angular.module('movieApp', ['ngRoute'])
 
 .config(function($routeProvider) {
-        $routeProvider
-            .when('/home', {
-                templateUrl: 'assets/home.html',
-                controller: 'homeCtrl'
-            })
-            .otherwise({
-                redirectTo: '/home'
-            });
-    })
-    .controller('homeCtrl', function ($scope, actorServ) {
-    	
-    	$('#searchButton').on('click',function(){
-    		$scope.movies = '';
-    		var actor = $('#actorText').val().toLowerCase();
-    		
-    		actorServ.getMovies(actor).then(function(data){
+	$routeProvider
+	.when('/home', {
+		templateUrl: 'assets/home.html',
+		controller: 'homeCtrl'
+	})
+	.otherwise({
+		redirectTo: '/home'
+	});
+}).controller('homeCtrl', function ($scope, actorServ, couchdbSrv) {
+
+	$('#searchButton').on('click',function(){
+		$scope.movies = '';
+		var actor = $('#actorText').val().toLowerCase();
+
+		/*imdbServ.getMovies(actor).then(function(data){
         		console.log('resolve from ctrl: ' + data.toString());
         		$scope.movies = data.filmography.actor;
         	},function(err){
         		console.log('ctrl promise error: ' + err);
-        	});
-    	});
-    })
-    .service('actorServ',function($http, $q){
-    	this.getMovies = function(actor) {
-    		var q = $q.defer();
-    		var url = 'http://theimdbapi.org/api/find/person?name=' + encodeURIComponent(actor);
-    		console.log(url);
-        	$http.get(url)
-        		.then(function(data){
-        			
-        			q.resolve(data.data[0]);
-        		},function(err){
-        			q.reject(err);
-        		});
-        	return q.promise;
-    	}
-    });
+        	});*/
+		couchdbSrv.getMovies(actor).then(function(data){
+			console.log('resolve from ctrl: ' + data.toString());
+			$scope.movies = data.filmography.actor;
+		},function(err){
+			imdbServ.getMovies(actor).then(function(data){
+				$scope.movies = data.filmography.actor;
+				couchdbSrv.postMovies(data);
+			}, function(err){});
+		});
+	});
+}).service('imdbServ',function($http, $q){
+	this.getMovies = function(actorName) {
+		var q = $q.defer();
+		var url = 'http://theimdbapi.org/api/find/person?name=' + encodeURIComponent(actorName);
+		console.log(url);
+		$http.get(url)
+		.then(function(data){
+			q.resolve(data.data[0]);
+		},function(err){
+			q.reject(err);
+		});
+		return q.promise;
+	}
+}).service('couchdbSrv',function($http, $q){
+	this.getMovies = function(actorName) {
+		var q = $q.defer();
+		var url = 'http://localhost:5984/students/' + actorName;
+		console.log(url);
+		$http.get(url)
+		.then(function(data){
+			if(true){
+				q.resolve(data.data);
+			}else{
+				q.reject('something');
+			}
+		},function(err){
+			q.reject(err);
+		});
+		return q.promise;
+	}
+	this.postMovies = function(actor){
+		var postData = {
+				'actor' : actor.title,
+				'movies' : actor.filmography.actor
+		};
+		$.ajax({
+			type: 'PUT',
+			url: '../../' + movies,
+			data: JSON.stringify(postData),
+			contentType: 'application/json',
+			dataType: "json",
+			success: function(data){
+				console.log('POST SUCCESS :' + data.toString());
+			},
+			error: function(XMLHttpRequest, textStatus, errorThrown){
+				console.log(errorThrown);
+			}
+		});
+	}
+});
