494c2c28a562b13e2622d4f1969d0aa8678ec4a6
FilipElen <filipelengit@outlook.com>
2017-11-08 15:30:24 +0100

    get movies from imdb works. probably going to change location in code later on.

--- app/_attachments/scripts/app.js
+++ app/_attachments/scripts/app.js
@@ -12,11 +12,32 @@ angular.module('movieApp', ['ngRoute'])
                 redirectTo: '/home'
             });
     })
-    .controller('homeCtrl', function ($scope) {
+    .controller('homeCtrl', function ($scope, actorServ) {
     	
     	$('#searchButton').on('click',function(){
+    		$scope.movies = '';
+    		var actor = $('#actorText').val().toLowerCase();
+    		
+    		actorServ.getMovies(actor).then(function(data){
+        		console.log('resolve from ctrl: ' + data.toString());
+        		$scope.movies = data.filmography.actor;
+        	},function(err){
+        		console.log('ctrl promise error: ' + err);
+        	});
     	});
     })
-    .service('addressServ',function($http, $q){
-
+    .service('actorServ',function($http, $q){
+    	this.getMovies = function(actor) {
+    		var q = $q.defer();
+    		var url = 'http://theimdbapi.org/api/find/person?name=' + encodeURIComponent(actor);
+    		console.log(url);
+        	$http.get(url)
+        		.then(function(data){
+        			
+        			q.resolve(data.data[0]);
+        		},function(err){
+        			q.reject(err);
+        		});
+        	return q.promise;
+    	}
     });
